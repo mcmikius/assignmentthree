fcdf7a1f4b006061d4f9f189c3296fea55ce1d32
Michail Bondarenko <mcmikius15@gmail.com>
2019-04-12 10:29:29 +0300

    first task is done

--- AssignmentThree/ViewController.swift
+++ AssignmentThree/ViewController.swift
@@ -9,11 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let myName = "Michail"
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        
+        let countName = nameCount(name: myName)
+        print("My name contains \(countName) characters.")
+        
+        
     }
-
+    
+//    1 создать строку с своим именемвывести количество символов содержащихся в ней
+    func nameCount(name: String) -> Int {
+        let count = name.count
+        return count
+    }
+//    2 создать строку с своим отчествомпроверить его на окончание “ич/на”(в классе написан метод который позволяет проверить на суффикс или префикс, найдите и используйте его)
 
 }
 
