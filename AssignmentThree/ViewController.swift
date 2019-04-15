673300c8f8171bde5b7d025ffb06c7cb8a977c2b
Michail Bondarenko <mcmikius15@gmail.com>
2019-04-15 12:00:48 +0300

    dataSource add

--- AssignmentThree/ViewController.swift
+++ AssignmentThree/ViewController.swift
@@ -202,15 +202,19 @@ class ViewController: UIViewController {
     //    8 написать метод который будет переводить строку в транслит - пример print(convertStrToTr4nslite(:”ЯЗЗЬ”)) -> “YAZZ”print(convertStrToTr4nslite:”морДа”) -> “morD4”
     
     
-    let dictionary = ["А": "A", "Б": "B", "В": "V", "Г": "G", "Д": "D", "Е": "E", "Ё": "E", "Ж": "Zh", "З": "Z", "И": "I", "Й": "I", "К": "K", "Л": "L", "М": "M", "Н": "N", "О": "O", "П": "P", "Р": "R", "С": "S", "Т": "T", "У": "U", "Ф": "F", "Х": "Kh", "Ц": "C", "Ч": "Ch", "Ш": "Sh", "Щ": "Sch", "Ъ": "'", "Ы": "Y", "Ъ": "'", "Э": "E", "Ю": "Yu", "Я": "Ya", "а": "a", "б": "b", "в": "v", "г": "g", "д": "d", "е": "e", "ё": "e", "ж": "zh", "з": "z", "и": "i", "й": "i", "к": "k", "л": "l", "м": "m", "н": "n", "о": "o", "п": "p", "р": "r", "с": "s", "т": "t", "у": "u", "ф": "f", "х": "h", "ц": "c", "ч": "ch", "ш": "sh", "щ": "sch", "ъ": "'", "ы": "y", "ъ": "'", "э": "e", "ю": "yu", "я": "ya"]
-    func convertStringToTranslite(string: String) {
-        for i in 0...string {
-            
-        }
-        
-    }
+    //    let dictionary = ["А": "A", "Б": "B", "В": "V", "Г": "G", "Д": "D", "Е": "E", "Ё": "E", "Ж": "Zh", "З": "Z", "И": "I", "Й": "I", "К": "K", "Л": "L", "М": "M", "Н": "N", "О": "O", "П": "P", "Р": "R", "С": "S", "Т": "T", "У": "U", "Ф": "F", "Х": "Kh", "Ц": "C", "Ч": "Ch", "Ш": "Sh", "Щ": "Sch", "Ъ": "'", "Ы": "Y", "Ъ": "'", "Э": "E", "Ю": "Yu", "Я": "Ya"]
+//    func convertStringToTranslite(string: String) {
+//        let latinString = string.applyingTransform(StringTransform.toLatin, reverse: false)
+//        let noDiacriticString = latinString?.applyingTransform(StringTransform.stripDiacritics, reverse: false)
+//    }
     
     
+    //    Задача 9. Сделать выборку из массива строк в которых содержится указанная строка
+    let dataSource = ["lada", "sedan", "baklazhan"]
+    let predicate = NSPredicate(format: "SELF contains %@", "da")
+    
+    
+    //    Задача 10. Set<String> - antimat [“fuck”, “fak”] “hello my fak” “hello my ***”
     
 }
 
